a61e95df441b2cb8b86dedc9d426869e4b5b48d2
luchunminglu <luchunminglu@gmail.com>
2016-10-06 18:10:44 +0800

    cc

---
+++ MyUpWorks/MyUpWorks/20161001/ParseXml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+
+public class ParseXml
+{
+    /// <summary>
+    /// True if Avaliable="true"
+    /// False if Avaliable="false"
+    /// null if Avaliable's value is Unknown
+    /// </summary>
+    /// <param name="xmlStr">The xml string need to parse</param>
+    /// <returns></returns>
+    public static bool? Parse(string xmlStr)
+    {
+        if (string.IsNullOrWhiteSpace(xmlStr))
+        {
+            return null;
+        }
+
+        xmlStr = xmlStr.Trim();
+        try
+        {
+            XElement xe = XElement.Parse(xmlStr);
+            XNamespace nameSpace = "http://api.namecheap.com/xml.response";
+            string result = xe.Element(nameSpace+"CommandResponse").Element(nameSpace+"DomainCheckResult").Attribute("Available").Value;
+            if (string.Compare(result, "false", StringComparison.CurrentCultureIgnoreCase) == 0)
+            {
+                return false;
+            }
+            else if (string.Compare(result, "true", StringComparison.CurrentCultureIgnoreCase) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+}
+
---
+++ MyUpWorks/MyUpWorks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyUpWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ParseXml.Parse(@"<ApiResponse Status=""OK"" xmlns=""http://api.namecheap.com/xml.response"">
+  <Errors />
+  <Warnings />
+  <RequestedCommand>namecheap.domains.check</RequestedCommand>
+  <CommandResponse>
+    <DomainCheckResult Domain=""google.com"" Available=""fAlse"" />
+  </CommandResponse>
+  <Server>WEB1-SANDBOX1</Server>
+  <GMTTimeDifference>--4:00</GMTTimeDifference>
+  <ExecutionTime>0.875</ExecutionTime>
+</ApiResponse>
+");
+            Console.WriteLine(result);
+        }
+    }
+}
